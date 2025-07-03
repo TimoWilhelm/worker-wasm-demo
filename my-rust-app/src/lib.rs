89cd09285088643a609bc417ca601a3329c27047
Timo Wilhelm <twilhelm@cloudflare.com>
2025-07-03 15:29:16 +0200

    add console log

--- my-rust-app/src/lib.rs
+++ my-rust-app/src/lib.rs
@@ -4,14 +4,16 @@ use wasm_bindgen::prelude::*;
 
 use crate::utils::set_panic_hook;
 
+use web_sys::console;
+
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
-	set_panic_hook();
-	Ok(())
+    set_panic_hook();
+	console::log_1(&"Hello using web-sys".into());
+    Ok(())
 }
-
 
 #[wasm_bindgen]
 pub fn greet(subject: &str) -> String {
-	format!("Hello, {}!", subject)
+    format!("Hello, {}!", subject)
 }
