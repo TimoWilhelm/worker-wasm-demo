7d16b956afb654519fb89e25bc34cc227a6e0246
Timo Wilhelm <twilhelm@cloudflare.com>
2025-07-03 12:18:58 +0200

    init sync with memory

--- my-rust-app/src/lib.rs
+++ my-rust-app/src/lib.rs
@@ -2,6 +2,13 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+use crate::utils::set_panic_hook;
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+	set_panic_hook();
+	Ok(())
+}
 
 
 #[wasm_bindgen]
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import mod from '../my-rust-app/pkg/my_rust_app_bg.wasm';
-import initWasm, { greet } from '../my-rust-app/pkg/my_rust_app';
-
-await initWasm(mod);
+import { greet, initSync } from '../my-rust-app/pkg/my_rust_app';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response(greet("World"));
+		const memory = new WebAssembly.Memory({ initial: 64, maximum: 64 });
+		initSync({ module: mod, memory });
+
+		return new Response(greet('World'));
 	},
 } satisfies ExportedHandler<Env>;
